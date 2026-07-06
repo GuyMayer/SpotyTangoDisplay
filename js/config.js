b27d0e2b406e32ff15cf08724743c3954fba69cd
GuyMayer <guy@zoom-photo.co.uk>
2026-07-06 22:46:40 +0100

    chore: bump version to 1.4.1

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     pollIntervalPaused: 5000,    // ms when paused/idle
   },
   app: {
-    version: '1.4.0',
+    version: '1.4.1',
     brand: 'TangoPassion',
     storagePrefix: 'spotd_',
   },
